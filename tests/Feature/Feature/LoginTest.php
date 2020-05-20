698a95ec1db6baaa113ca4e0b4f970c62c2e2db6
Bola Bally <bolabally@gmail.com>
2020-05-20 10:08:05 +0000

    addLoginTest.php

--- tests/Feature/Feature/LoginTest.php
+++ tests/Feature/Feature/LoginTest.php
@@ -8,15 +8,38 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LoginTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testRequiresEmailAndLogin()
     {
-        $response = $this->get('/');
+        $this->json('POST', 'api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                'email' => ['The email field is required.'],
+                'password' => ['The password field is required.'],
+            ]);
+    }
 
-        $response->assertStatus(200);
+
+    public function testUserLoginsSuccessfully()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'testlogin@user.com',
+            'password' => bcrypt('toptal123'),
+        ]);
+
+        $payload = ['email' => 'testlogin@user.com', 'password' => 'toptal123'];
+
+        $this->json('POST', 'api/login', $payload)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at',
+                    'api_token',
+                ],
+            ]);
+
     }
 }
