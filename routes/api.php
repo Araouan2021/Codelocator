751c38bce939cf08d45438f4d8b15f12565d62b3
Bola Bally <bolabally@gmail.com>
2020-05-29 11:12:04 +0000

    addapi.php

--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', 'Auth\LoginController@login');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware'=>'auth'], function(){
+
+Route::post('login', 'Auth\LoginController@login')->name('login');
 Route::post('logout', 'Auth\LoginController@logout');
 Route::post('register', 'Auth\RegisterController@register');
 
-//Route::group(['middleware' => 'auth:api'], function() {
+Route::group(['middleware' => 'auth:api'], function() {
 Route::get('records', 'RecordController@index');
 Route::get('records/{record}', 'RecordController@show');
 Route::post('records', 'RecordController@store');
 Route::put('records/{record}', 'RecordController@update');
 Route::delete('records/{records}', 'RecordController@delete');
-//});
+});
